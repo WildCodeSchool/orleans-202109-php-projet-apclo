cc8131fde9b541ed43e8f107d634045c2323fe7a
Estelle Weisgerber <akumaony@gmail.com>
2021-11-03 16:08:51 +0100

    last issues resolved

--- database.sql
+++ database.sql
@@ -10,18 +10,4 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
--- CREATION DATABASE `association`
---
--- CREATION TABLE `fur`, `gender`, `breed`, `color`, `adopter`
-
-/*CREATE TABLE `fur` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`));
-
-CREATE TABLE `gender` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`));
-
-CREATE TABLE `breed` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`));
-
-CREATE TABLE `color` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`));
-
-CREATE TABLE `adopter` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, `firstname` VARCHAR(100) NOT NULL, `email` VARCHAR(100) NOT NULL, `adress` VARCHAR(255) NOT NULL, `phone_number` CHAR(30), PRIMARY KEY (`id`));*/
-
 CREATE TABLE `cat` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, `digital_chip` INT(15), `description` TEXT, `adoption_date` DATE, `birth_date` DATE, PRIMARY KEY (`id`);

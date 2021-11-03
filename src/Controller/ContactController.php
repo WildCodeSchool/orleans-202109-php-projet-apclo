cf22dce7459e190ac0d34dc0db9a7736f54d518e
Martin Lerude <martin.lerude@gmail.com>
2021-11-03 17:11:14 +0100

    form_without_bootstrap

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+class ContactController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Contact/index.html.twig');
+    }
+}

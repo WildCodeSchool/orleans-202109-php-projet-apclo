f27554a95a63a6e1509451a1a05a1e07718c82e6
Martin Lerude <martin.lerude@gmail.com>
2021-11-03 23:09:34 +0100

    form_little_update

--- src/View/Contact/thanks.php
+++ src/View/Contact/thanks.php
@@ -1,62 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Réponse du Formulaire</title>
 </head>
+
 <body>
-    
-<?php
 
-$errors="";
+    <?php
 
-if (empty($_POST["firstname"])){
+    $errors = "";
 
-    $errors.= "Le nom est requis </br>" . PHP_EOL;
-}
+    if (empty($_POST["firstname"])) {
+        $errors .= "Le nom est requis </br>" . PHP_EOL;
+    }
 
-if (empty($_POST["lastname"])){
+    if (empty($_POST["lastname"])) {
+        $errors .= "Le prénom est requis </br>" . PHP_EOL;
+    }
 
-    $errors.= "Le prénom est requis </br>" . PHP_EOL;
-}
+    if (empty($_POST["e-mail"])) {
+        $errors .= "L'E-mail est requis </br>" . PHP_EOL;
+    }
 
-if (empty($_POST["e-mail"])){
+    if (empty($_POST["tel"])) {
+        $errors .= "Le numéro de téléphone est requis </br>" . PHP_EOL;
+    }
 
-    $errors.= "L'E-mail est requis </br>" . PHP_EOL;
-}
+    if (empty($_POST["message"])) {
+        $errors .= "Vous devez écrire une message pour pouvoir valider </br>" . PHP_EOL;
+    }
 
-if (empty($_POST["tel"])){
+    if (empty($_POST["subject"])) {
+        $errors .= "Vous devez choisir un sujet </br>" . PHP_EOL;
+    }
 
-    $errors.= "Le numéro de téléphone est requis </br>" . PHP_EOL;
-}
+    if (empty($errors)) {
+        ?>
 
-if (empty($_POST["message"])){
+        }
+        <p>Merci <?php echo $_POST["firstname"] . " "; ?><?php echo $_POST["lastname"] . " "; ?> 
+        de nous avoir contacté à propos de <?php echo $_POST["subject"] . " "; ?>.
 
-    $errors.= "Vous devez écrire une message pour pouvoir valider </br>" . PHP_EOL;
-}
+            Un des membres de l'APCLO vous contactera soit à l'adresse <?php echo $_POST['e-mail'] . " "; ?> 
+            ou par téléphone au <?php echo $_POST["tel"] . " "; ?> dans les plus court délais, 
+            afin de traiter votre demande :
 
-if (empty($_POST["subject"])){
+            <?php echo $_POST["message"]; ?>
+        </p>
 
-    $errors.= "Vous devez choisir un sujet </br>" . PHP_EOL;
-}
-
-if (empty($errors)){
-?>
-
-}
-<p>Merci <?php echo $_POST ["firstname"] . " " ; ?><?php echo $_POST ["lastname"] . " ";?> de nous avoir contacté à propos de <?php echo $_POST["subject"] . " "; ?>.
-
-Un des membres de l'APCLO vous contactera soit à l'adresse <?php echo $_POST['e-mail'] . " ";?> ou par téléphone au <?php echo $_POST ["tel"] . " ";?> dans les plus court délais, afin de traiter votre demande :
-
-<?php echo $_POST ["message"]; ?> 
-</p>
-
-<?php }else{
-
-    echo $errors;
-}?>
+    <?php } else {
+        echo $errors;
+    } ?>
 
 </body>
+
 </html>

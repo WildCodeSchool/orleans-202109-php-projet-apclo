1037660f3a6fcc2f0985f9e142958ca531de5156
Estelle Weisgerber <akumaony@gmail.com>
2021-11-08 11:24:41 +0100

    image size, responsive card, and displayed date changed

--- src/Model/CatManager.php
+++ src/Model/CatManager.php
@@ -8,8 +8,9 @@ class CatManager extends AbstractManager
 
     public function selectOneById(int $id)
     {
-        $statement = $this->pdo->prepare("SELECT cat.name as name, image, birth_date, digital_chip,
-        description, adoption_date, gender.name as gender, fur.length as length,
+        $statement = $this->pdo->prepare("SELECT cat.name as name, image, 
+        ROUND((DATEDIFF(NOW(), birth_date) / 365)) as age,
+        digital_chip, description, adoption_date, gender.name as gender, fur.length as length,
         color.name as color, breed.name as breed FROM " .
         self::TABLE . " LEFT JOIN gender ON gender.id = cat.gender_id JOIN fur ON fur.id = cat.fur_id
         JOIN breed ON breed.id = cat.breed_id JOIN color ON color.id = cat.color_id WHERE cat.id=:id");

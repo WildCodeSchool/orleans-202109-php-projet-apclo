ebe5f569cc85fcce990f216877645ad45b564667
Djoa <siongdjoa3@gmail.com>
2021-11-08 11:38:43 +0100

    ajout séparateur pattes de chats et création query pour afficher les actualités en ordre décroissant

--- src/Controller/ActualityController.php
+++ src/Controller/ActualityController.php
@@ -9,7 +9,8 @@ class ActualityController extends AbstractController
     public function index()
     {
         $actualityManager = new ActualityManager();
-        $actualities = $actualityManager->selectAll($orderBy="date",$direction='DESC');
+        $actualities = $actualityManager->selectAll("date", "DESC");
+        
         return $this->twig->render('Actualities/index.html.twig', ['actualities' => $actualities]);
     }
 }

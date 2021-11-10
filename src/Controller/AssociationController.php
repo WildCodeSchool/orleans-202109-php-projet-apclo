ea5f32ee0d91dfb1bf23e4fd6ae42c2b0b45d290
Martin Lerude <martin.lerude@gmail.com>
2021-11-10 15:10:37 +0100

    add_css

--- src/Controller/AssociationController.php
+++ src/Controller/AssociationController.php
@@ -4,14 +4,6 @@ namespace App\Controller;
 
 class AssociationController extends AbstractController
 {
-    /**
-     * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
     public function index()
     {
         return $this->twig->render('Association/index.html.twig');

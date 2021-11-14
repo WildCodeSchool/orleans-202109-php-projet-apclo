4492e50f347f151692cdcb6f85fc3258180ea113
Martin Lerude <martin.lerude@gmail.com>
2021-11-14 14:09:19 +0100

    form safety

--- src/Model/StampManager.php
+++ src/Model/StampManager.php
@@ -10,13 +10,13 @@ class StampManager extends AbstractManager
 
     public function insert(array $stamp): int
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (lastname, firstname, tel, email, subject) VALUES (:lastname, :firstname, :tel, :email, :subject)");
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (lastname, firstname, tel, email, subject) 
+        VALUES (:lastname, :firstname, :tel, :email, :subject)");
         $statement->bindValue('title', $stamp['lastname'], \PDO::PARAM_STR);
         $statement->bindValue('title', $stamp['firstname'], \PDO::PARAM_STR);
         $statement->bindValue('title', $stamp['tel'], \PDO::PARAM_STR);
         $statement->bindValue('title', $stamp['email'], \PDO::PARAM_STR);
         $statement->bindValue('title', $stamp['subject'], \PDO::PARAM_STR);
-        
 
         $statement->execute();
         return (int)$this->pdo->lastInsertId();

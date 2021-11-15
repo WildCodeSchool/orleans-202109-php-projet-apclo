e4fa0ccb12970c654bf431f6fa20e5aa92b24ce8
Martin Lerude <martin.lerude@gmail.com>
2021-11-15 11:25:21 +0100

    fix catmanager

--- src/Model/CatManager.php
+++ src/Model/CatManager.php
@@ -51,4 +51,18 @@ class CatManager extends AbstractManager
 
         return $this->pdo->query($query)->fetchAll();
     }
+
+    public function insert(array $cat): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (lastname, firstname, tel, email, subject) 
+        VALUES (:lastname, :firstname, :tel, :email, :subject)");
+        $statement->bindValue('title', $cat['lastname'], \PDO::PARAM_STR);
+        $statement->bindValue('title', $cat['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue('title', $cat['tel'], \PDO::PARAM_STR);
+        $statement->bindValue('title', $cat['email'], \PDO::PARAM_STR);
+        $statement->bindValue('title', $cat['subject'], \PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }

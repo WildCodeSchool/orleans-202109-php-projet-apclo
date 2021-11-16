53da3a67729281927fcee02b94e7b3cd8bf966ac
Martin Lerude <martin.lerude@gmail.com>
2021-11-16 21:26:41 +0100

    correction AdminCatController

---
+++ src/Model/ContactManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Model;
+
+class ContactManager extends AbstractManager
+{
+    public const TABLE = 'cat';
+    public function insert(array $contact): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (lastname, firstname, tel, email, subject)
+    VALUES (:lastname, :firstname, :tel, :email, :subject)");
+        $statement->bindValue('lastname', $contact['lastname'], \PDO::PARAM_STR);
+        $statement->bindValue('firstname', $contact['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue('tel', $contact['tel'], \PDO::PARAM_STR);
+        $statement->bindValue('email', $contact['email'], \PDO::PARAM_STR);
+        $statement->bindValue('subject', $contact['subject'], \PDO::PARAM_STR);
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+}

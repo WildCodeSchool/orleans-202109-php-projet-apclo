6e2f3bdbfc7883bbe6b84002c7d27c2d9c064f2d
Arthur GOUAUX <arthur.gouaux@gmail.com>
2021-11-16 22:05:59 +0100

    deleted showMembers function

--- src/Model/MemberManager.php
+++ src/Model/MemberManager.php
@@ -5,11 +5,4 @@ namespace App\Model;
 class MemberManager extends AbstractManager
 {
     public const TABLE = 'member';
-
-    public function showMembers()
-    {
-        $query = " SELECT * FROM " . self::TABLE;
-
-        return $this->pdo->query($query)->fetchAll();
-    }
 }

dbbaa756f94825b713d36567ecce8ed5f6fdfd6e
Estelle Weisgerber <akumaony@gmail.com>
2021-11-16 16:08:40 +0100

    fixed syntax error

--- src/Model/CatManager.php
+++ src/Model/CatManager.php
@@ -34,7 +34,7 @@ class CatManager extends AbstractManager
 
     public function selectAllCats(): array
     {
-        $query = "SELECT cat.name as name, image, birth_date, gender.name as gender, cat.id as id FROM "
+        $query = "SELECT cat.name as name, image, birth_date, gender.name as gender, cat.id as id FROM " .
         self::TABLE . " JOIN gender ON gender.id = cat.gender_id";
 
         return $this->pdo->query($query)->fetchAll();

403e8c42dbee3de2f3098096fe0c44e85bc68531
Martin Lerude <martin.lerude@gmail.com>
2021-11-16 17:08:55 +0100

    fix ContactControllerManager

--- src/Model/ContactManager.php
+++ src/Model/ContactManager.php
@@ -2,15 +2,12 @@
 
 namespace App\Model;
 
-use App\Controller\AbstractController;
-
-class ContactControllerManager extends AbstractController
+class ContactManager extends AbstractManager
 {
     public const TABLE = 'cat';
-    
     public function insert(array $contact): int
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (lastname, firstname, tel, email, subject) 
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (lastname, firstname, tel, email, subject)
     VALUES (:lastname, :firstname, :tel, :email, :subject)");
         $statement->bindValue('lastname', $contact['lastname'], \PDO::PARAM_STR);
         $statement->bindValue('firstname', $contact['firstname'], \PDO::PARAM_STR);
